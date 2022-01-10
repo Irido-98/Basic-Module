661dca4b7df80733c3ba7d44e3b515d424af4ab5
Irodi <chirag.thakur04@outlook.com>
2022-01-10 18:24:08 +0000

    Added recursive descent parser

---
+++ Parsetest/interpreter.py
@@ -0,0 +1,40 @@
+from values import Number
+from functions import function
+import math
+
+
+class Interpreter:
+    def visit(self, node):  # Pass in the root node here
+        method_name = f'visit_{type(node).__name__}'  # This tells us the type of node the node we are on is
+        method = getattr(self, method_name)
+        return method(node)
+
+    def visit_NumberNode(self, node):
+        return Number(node.value)
+
+    def visit_AddNode(self, node):
+        return Number(self.visit(node.node_a).value + self.visit(node.node_b).value)
+
+    def visit_SubtractNode(self, node):
+        return Number(self.visit(node.node_a).value - self.visit(node.node_b).value)
+
+    def visit_MultiplyNode(self, node):
+        return Number(self.visit(node.node_a).value * self.visit(node.node_b).value)
+
+    def visit_DivideNode(self, node):
+        try:
+            return Number(self.visit(node.node_a).value / self.visit(node.node_b).value)
+        except:
+            raise Exception('Runtime math error')
+
+    def visit_PlusNode(self, node):
+        return self.visit(node.node)
+
+    def visit_MinusNode(self, node):
+        return Number(-self.visit(node.node).value)
+
+    def visit_IndiceNode(self, node):
+        return Number(self.visit(node.node_a).value ** self.visit(node.node_b).value)
+
+    def visit_SinNode(self, node):
+        return Number(function.sin(self.visit((node.node).value)))
